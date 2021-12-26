20a52b2bbf4a2c8be61b34a5970945003e6997a2
DENISA-MONICA LOBONŢ <denisa.lobont@stud.ubbcluj.ro>
2021-12-26 22:56:51 +0200

    lab11

--- Lobont_Denisa_Lab10/Models/Product.cs
+++ Lobont_Denisa_Lab10/Models/Product.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Lobont_Denisa_Lab10.Models
 {
-    public class ShopList
+    public class Product
     {
+
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
     }
 }
